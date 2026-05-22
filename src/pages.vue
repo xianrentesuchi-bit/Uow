8446cc4e30a27668a6782ce8c617e15e1fd2e74b
xianrentesuchi-bit <xianrentesuchi@gmail.com>
2026-05-22 21:19:07 +0900

    Create pages.vue

---
+++ src/pages.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import Layout from '../components/layout/Layout.vue'
+import VideoGrid from '../components/video/VideoGrid.vue'
+import { trendingVideos } from '../api/invidious'
+
+const videos = ref<any[]>([])
+
+onMounted(async () => {
+  videos.value = await trendingVideos()
+})
+</script>
+
+<template>
+  <Layout>
+    <VideoGrid :videos="videos" />
+  </Layout>
+</template>
