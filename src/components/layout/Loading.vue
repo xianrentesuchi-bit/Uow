4f0d7f898d6d022f84fded3f9795fbc574b37f1c
senninsugar <hiroki20134@gmail.com>
2026-05-23 18:11:47 +0900

    Update Loading.vue

--- src/components/layout/Loading.vue
+++ src/components/layout/Loading.vue
@@ -1,19 +1,30 @@
+<script setup lang="ts">
+import Header from './Header.vue'
+import Sidebar from './Sidebar.vue'
+</script>
+
 <template>
-  <div class="fixed inset-0 z-[9999] bg-[#f9f9f9] flex items-center justify-center">
-    <div class="flex flex-col items-center">
+  <Header />
 
-      <div
-        class="w-16 h-16 border-4 border-zinc-200 border-t-red-500 rounded-full animate-spin"
-      ></div>
+  <Sidebar />
 
-      <div class="mt-6 text-xl font-semibold text-zinc-900">
-        動画読み込み中...
+  <div class="pt-14 pl-60">
+    <div class="fixed inset-0 z-[9999] bg-[#f9f9f9] flex items-center justify-center">
+      <div class="flex flex-col items-center">
+
+        <div
+          class="w-16 h-16 border-4 border-zinc-200 border-t-red-500 rounded-full"
+        ></div>
+
+        <div class="mt-6 text-xl font-semibold text-zinc-900">
+          動画読み込み中...
+        </div>
+
+        <div class="mt-2 text-zinc-500">
+          動画検索やデータ取得をしています
+        </div>
+
       </div>
-
-      <div class="mt-2 text-zinc-500">
-        動画検索やデータ取得をしています
-      </div>
-
     </div>
   </div>
 </template>
